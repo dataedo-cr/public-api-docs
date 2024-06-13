73f6ed9d44778e1447ab1ba0bb95f478f045fa21
Maciej Foryś <maciej.forys@dataedo.com>
2024-06-13 08:33:21 +0200

    init

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "./public_api_docs.json",
+    url: "dist/public_api_docs.json",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
