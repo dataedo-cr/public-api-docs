61732bcd19e07cea3f6e1c70a4e4ef0228930e45
Maciej Foryś <maciej.forys@dataedo.com>
2024-06-13 08:19:30 +0200

    init

---
+++ dist/swagger-initializer.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+  //<editor-fold desc="Changeable Configuration Block">
+
+  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
+  window.ui = SwaggerUIBundle({
+    url: "./public_api_docs.json",
+    dom_id: '#swagger-ui',
+    deepLinking: true,
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      SwaggerUIStandalonePreset
+    ],
+    plugins: [
+      SwaggerUIBundle.plugins.DownloadUrl
+    ],
+    supportedSubmitMethods: [],
+    layout: "StandaloneLayout"
+  });
+
+  //</editor-fold>
+};
